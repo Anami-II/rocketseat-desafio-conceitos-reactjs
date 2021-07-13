44f13a386d0aec8578949f8d941599b1a1136e48
Eduardo Anami <duhsegundo@hotmail.com>
2021-07-13 03:12:22 -0300

    feat: Adicionar e remover repositórios.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import api from "./services/api";
 
 import "./styles.css";
 
 function App() {
+
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(({ data }) => {
+      setProjects(data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const { data: project } = await api.post('repositories', {
+      title: `Repositório ${new Date()}`,
+      owner: 'Eduardo Anami'
+    });
+
+    const newProjects = [...projects, project];
+
+    setProjects(newProjects);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    try {
+      const response = await api.delete(`repositories/${id}`);
+
+      if (response.status !== 204) {
+        throw new Error('Não foi possível exluir o projeto.');
+      }
+
+      const newProjects = [...projects];
+
+      const projectIndex = newProjects
+        .findIndex(project => project.id === id);
+
+      newProjects.splice(projectIndex, 1);
+
+      setProjects(newProjects);
+    } catch (e) {
+      alert(e.message);
+    }
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {
+          projects.map(repository => (
+            <li key={repository.id}>
+              { repository.title}
+
+              <button onClick={() => handleRemoveRepository(repository.id)}>
+                Remover
+              </button>
+            </li>
+          ))
+        }
+
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
